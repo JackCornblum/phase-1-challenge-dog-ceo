c612931e5ff4f638f92d24c331b8c698b93f8d27
jackcornblum <jcornblum@gmail.com>
2021-05-25 15:54:33 -0400

    second and third challenges complete

--- src/index.js
+++ src/index.js
@@ -23,7 +23,43 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector("ul#dog-breeds").append(li);
     }
 
+    
+
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"
+
+    function fetchBreed(url) {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => secondChallenge(data.message))
+    }
+
+    function secondChallenge(object) {
+        //console.log(object)
+    
+        for (const key in object) {
+            //console.log(key);
+            //console.log(object[key]);
+            object[key].forEach((arr) => {
+                let li = document.createElement("li");
+                if (arr === "") {
+                    return
+                }
+                else {
+                    let breed = arr + " " + key;
+                    li.append(breed);
+                    li.addEventListener("click", () => li.style.color = "#ce42f5" )
+                    document.querySelector("ul#dog-breeds").append(li);
+
+                }
+            })
+        }
+
+    }
+
     fetchImg(imgUrl);
+    fetchBreed(breedUrl);
 
 })
+
+
 
