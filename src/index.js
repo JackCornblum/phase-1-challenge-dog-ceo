888f8e4ba908b64b50ff8143b2c323d50bcca159
jackcornblum <jcornblum@gmail.com>
2021-05-25 15:00:37 -0400

    challenge 1 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+    function fetchImg(url) {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => data.message.forEach(appendDog))
+
+    
+    }
+
+    function appendDog(dogsUrl) {
+        let li = document.createElement("li");
+        li.className = "dogs";
+
+        let img = document.createElement("img");
+        img.src = dogsUrl;
+
+        li.append(img)
+        document.querySelector("ul#dog-breeds").append(li);
+    }
+
+    fetchImg(imgUrl);
+
+})
+
